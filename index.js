dada1b9f7e3fbb7a05b544ca9fddd8d72ea6e1ef
Ivan Donyk <ivandonyk@gmail.com>
2021-01-21 22:42:41 -0500

    make an entry for the zipcode ranges

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var axios = require('axios');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 var now = new Date();
-var shippingDate = new Date(now.getTime() + now.getTimezoneOffset() * 60000).toLocaleDateString("en-us")+"&_="+ now.getTime();
+var utcNow = new Date(now.getTime() + now.getTimezoneOffset() * 60000);
+var shippingDate = utcNow.toLocaleDateString("en-us")+"&_="+ now.getTime();
 
 const csvWriter = createCsvWriter({
-	path: `./zipcode_${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}.csv`,
+	path: `./zipcode_${utcNow.getFullYear()}-${utcNow.getMonth()+1}-${utcNow.getDate()}.csv`,
 	header: [
 			{id: 'origin', title: 'Origin'},
 			{id: 'destination', title: 'Destination'},
@@ -13,53 +14,77 @@ const csvWriter = createCsvWriter({
 	]
 });
 
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
+async function makeRecord(origin, recData) {
+	const record = [];
+	const zone = recData.Zone;
+	const zipcodes = recData.ZipCodes;
+	if (zipcodes.indexOf("---")>-1) {
+		const zip_arr = zipcodes.split("---");
+		const first_zip = Number(zip_arr[0]);
+		const last_zip = Number(zip_arr[1]);
+		for (let i = first_zip; i<=last_zip; i++){
+			record.push({
+				origin: origin,
+				destination: ('000' + i).substr(-3),
+				zone
+			})
+		}
+	} else {
+		record.push({
+			origin,
+			destination: zipcodes,
+			zone
+		})
+	}
+
+	return record;
+}
 
 async function makeCSV(zipcode, shippingDate){
 	const records = [];
-	axios.get(
+	return axios.get(
 		`https://postcalc.usps.com/DomesticZoneChart/GetZoneChart?zipCode3Digit=${zipcode}&shippingDate=${shippingDate}`
-	).then(res => {
-		res.data.Column0.forEach(el => {
-			records.push({
-				origin: zipcode,
-				destination: el.ZipCodes,
-				zone: el.Zone
-			});
+	).then(async(res) => {
+		await asyncForEach(res.data.Column0, async(el) => {
+			const rec = await makeRecord(zipcode, el);
+			records.push(...rec);
 		});
 
-		res.data.Column1.forEach(el => {
-			records.push({
-				origin: zipcode,
-				destination: el.ZipCodes,
-				zone: el.Zone
-			});
+		await asyncForEach(res.data.Column1, async(el) => {
+			const rec = await makeRecord(zipcode, el)
+			records.push(...rec);
 		});
 
-		res.data.Column2.forEach(el => {
-			records.push({
-				origin: zipcode,
-				destination: el.ZipCodes,
-				zone: el.Zone
-			});
+		await asyncForEach(res.data.Column2, async(el) => {
+			const rec = await makeRecord(zipcode, el)
+			records.push(...rec);
 		});
 		
-		res.data.Column3.forEach(el => {
-			records.push({
-				origin: zipcode,
-				destination: el.ZipCodes,
-				zone: el.Zone
-			});
+		await asyncForEach(res.data.Column3, async(el) => {
+			const rec = await makeRecord(zipcode, el)
+			records.push(...rec);
 		});
 
-		csvWriter.writeRecords(records)
-    .then(() => {
-        console.log(zipcode + ': Done');
-		});
+		return records;	
 		
-	}).catch((e)=>console.log(e));
+	}).catch((e)=>console.error(e));
 }
 
-for(let i = 0; i<1000; i++){
+
+asyncForEach(Array.from(Array(1000).keys()), async (i) => {
 	const zipcode = ('000' + i).substr(-3);
-	makeCSV(zipcode, shippingDate);
-}
+	
+	const re = await makeCSV(zipcode, shippingDate);
+	if(re.length > 0) {
+		csvWriter.writeRecords(re)
+		.then(() => {
+				console.log(zipcode + ': Saved');
+		});
+	}
+});
